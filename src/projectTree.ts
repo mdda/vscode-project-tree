6a5053e3a40d2c78c6c54ddbd6248ac40bf25d97
Martin Andrews <GitHub@mdda.net>
2018-12-30 07:07:13 +0800

    First real output

--- src/projectTree.ts
+++ src/projectTree.ts
@@ -26,12 +26,16 @@ let optsTopLevel: number[] = [];
 let optsExpandNodes: number[] = [];
 let optsDoSort = true;
 
-export class SymbolNode {
-  parent?: SymbolNode;
-  symbol: SymbolInformation;
-  children: SymbolNode[];
+export class ProjectTreeNode {
+  parent?: ProjectTreeNode;
+  name:    string;
+  path:    string;
+  symbol:  SymbolInformation;
+  children: ProjectTreeNode[];
 
-  constructor(symbol?: SymbolInformation) {
+  constructor(name:string, path:string, symbol?: SymbolInformation) {
+    this.name=name;
+    this.path=path;
     this.children = [];
     this.symbol = symbol;
   }
@@ -55,7 +59,7 @@ export class SymbolNode {
     return ix;
   }
 
-  private compareSymbols(a: SymbolNode, b: SymbolNode): number {
+  private compareSymbols(a: ProjectTreeNode, b: ProjectTreeNode): number {
     const kindOrder =
       this.getKindOrder(a.symbol.kind) - this.getKindOrder(b.symbol.kind);
     if (kindOrder !== 0) {
@@ -73,24 +77,27 @@ export class SymbolNode {
     //this.children.forEach(child => child.sort());
   }
 
-  addChild(child: SymbolNode) {
+  addChild(child: ProjectTreeNode) {
     child.parent = this;
     this.children.push(child);
   }
 }
 
-export class ProjectTreeTreeDataProvider
-  implements TreeDataProvider<SymbolNode> {
-  private _onDidChangeTreeData: EventEmitter<SymbolNode | null> = new EventEmitter<SymbolNode | null>();
-  readonly onDidChangeTreeData: Event<SymbolNode | null> = this._onDidChangeTreeData.event;
+export class ProjectTreeTreeDataProvider implements TreeDataProvider<ProjectTreeNode> {
+  private _onDidChangeTreeData: EventEmitter<ProjectTreeNode | null> = new EventEmitter<ProjectTreeNode | null>();
+  readonly onDidChangeTreeData: Event<ProjectTreeNode | null> = this._onDidChangeTreeData.event;
 
   private context: ExtensionContext;
-  private tree: SymbolNode;
+  private tree:   ProjectTreeNode;
   private editor: TextEditor;
 
   constructor(context: ExtensionContext) {
     console.log("ProjectTreeTreeDataProvider.constructor");
     this.context = context;
+  }
+  
+  setTreeData(root: ProjectTreeNode) {
+    this.tree=root;
   }
 
   private getSymbols(document: TextDocument): Thenable<SymbolInformation[]> {
@@ -102,7 +109,7 @@ export class ProjectTreeTreeDataProvider
     );
   }
 
-  private compareSymbols(a: SymbolNode, b: SymbolNode) {
+  private compareSymbols(a: ProjectTreeNode, b: ProjectTreeNode) {
     const startComparison = a.symbol.location.range.start.compareTo(
       b.symbol.location.range.start
     );
@@ -112,10 +119,11 @@ export class ProjectTreeTreeDataProvider
     return b.symbol.location.range.end.compareTo(a.symbol.location.range.end);
   }
 
+
   private async updateSymbols(editor: TextEditor): Promise<void> {
     console.log("ProjectTreeTreeDataProvider.updateSymbols");
     
-    const tree = new SymbolNode();
+    const tree = new ProjectTreeNode("","");
     this.editor = editor;
     if (editor) {
       readOpts();
@@ -124,12 +132,12 @@ export class ProjectTreeTreeDataProvider
         symbols = symbols.filter(sym => optsTopLevel.indexOf(sym.kind) >= 0);
       }
       // Create symbol nodes
-      const symbolNodes = symbols.map(symbol => new SymbolNode(symbol));
+      const ProjectTreeNodes = symbols.map(symbol => new ProjectTreeNode("","", symbol));
       // Sort nodes by left edge ascending and right edge descending
-      symbolNodes.sort(this.compareSymbols);
+      ProjectTreeNodes.sort(this.compareSymbols);
       // Start with an empty list of parent candidates
-      let potentialParents: SymbolNode[] = [];
-      symbolNodes.forEach(currentNode => {
+      let potentialParents: ProjectTreeNode[] = [];
+      ProjectTreeNodes.forEach(currentNode => {
         // Drop candidates that do not contain the current symbol range
         potentialParents = potentialParents
           .filter(
@@ -160,24 +168,25 @@ export class ProjectTreeTreeDataProvider
     this.tree = tree;
   }
 
-  async getChildren(node?: SymbolNode): Promise<SymbolNode[]> {
-    console.log("ProjectTreeTreeDataProvider.getChildren");
+  async getChildren(node?: ProjectTreeNode): Promise<ProjectTreeNode[]> {
+    console.log("ProjectTreeTreeDataProvider.getChildren", node);
 
     if (node) {
       return node.children;
     } else {
-      await this.updateSymbols(window.activeTextEditor);
-      return this.tree ? this.tree.children : [];
+      //await this.updateSymbols(window.activeTextEditor);
+      //return this.tree ? this.tree.children : [];
+      return this.tree.children; // root returns list of children
     }
   }
 
-  getParent(node: SymbolNode): SymbolNode {
+  getParent(node: ProjectTreeNode): ProjectTreeNode {
     console.log("ProjectTreeTreeDataProvider.getParent");
     
     return node.parent;
   }
 
-  getNodeByPosition(position: Position): SymbolNode {
+  getNodeByPosition(position: Position): ProjectTreeNode {
     console.log("ProjectTreeTreeDataProvider.getNodeByPosition", position);
     
     let node = this.tree;
@@ -195,7 +204,32 @@ export class ProjectTreeTreeDataProvider
     }
   }
 
-  getTreeItem(node: SymbolNode): TreeItem {
+  getTreeItem(node: ProjectTreeNode): TreeItem {
+    console.log("ProjectTreeTreeDataProvider.getTreeItem");
+    
+    //const { kind } = node.symbol;
+    let kind = SymbolKind.Namespace;
+    let treeItem = new TreeItem(node.name);
+
+    if (node.children.length) {
+      treeItem.collapsibleState = TreeItemCollapsibleState.Collapsed;
+    } 
+    else {
+      treeItem.collapsibleState = TreeItemCollapsibleState.None;
+      kind = SymbolKind.Method;
+      treeItem.command = {
+        command: "projectTree.openFile",
+        title: "Open",
+        arguments: [this.editor, node.path]
+      };
+    }
+
+
+    treeItem.iconPath = getIcon(kind, this.context);
+    return treeItem;
+  }
+  
+  getTreeItem_orig(node: ProjectTreeNode): TreeItem {
     console.log("ProjectTreeTreeDataProvider.getTreeItem");
     
     const { kind } = node.symbol;
@@ -203,10 +237,11 @@ export class ProjectTreeTreeDataProvider
 
     if (node.children.length) {
       treeItem.collapsibleState =
-        optsExpandNodes.length && SymbolNode.shouldAutoExpand(kind)
+        optsExpandNodes.length && ProjectTreeNode.shouldAutoExpand(kind)
           ? TreeItemCollapsibleState.Expanded
           : TreeItemCollapsibleState.Collapsed;
-    } else {
+    } 
+    else {
       treeItem.collapsibleState = TreeItemCollapsibleState.None;
     }
 
@@ -227,13 +262,22 @@ export class ProjectTreeTreeDataProvider
 }
 
 export class ProjectTreeProvider {
-  projectViewer: TreeView<SymbolNode>;
+  projectViewer: TreeView<ProjectTreeNode>;
 
   constructor(context: ExtensionContext) {
     console.log("ProjectTreeProvider.constructor");
     
     const treeDataProvider = new ProjectTreeTreeDataProvider(context);
     console.log("ProjectTreeProvider.constructor - created");
+    
+    let root = new ProjectTreeNode("ROOT", "INGORE");
+    let group1 = new ProjectTreeNode("Hello", "NOOO");
+    group1.addChild(new ProjectTreeNode("Hello-sub1", "hello.js"))
+    group1.addChild(new ProjectTreeNode("Hello-sub2", "hello.js"))
+    root.addChild(group1)
+    root.addChild(new ProjectTreeNode("Hello2", "hello2.js"))
+    
+    treeDataProvider.setTreeData(root);
     
     this.projectViewer = window.createTreeView("projectTree", {
       treeDataProvider
@@ -244,6 +288,13 @@ export class ProjectTreeProvider {
       treeDataProvider.refresh();
     });
     console.log("ProjectTreeProvider.constructor - registered refresh command");
+
+    commands.registerCommand(
+      "projectTree.openFile",
+      (editor: TextEditor, path: string) => {
+        console.log("command : projectTree.openFile firing", path);
+      }
+    );
     
     /*
     commands.registerCommand(
