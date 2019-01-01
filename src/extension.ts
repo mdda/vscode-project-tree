89967fa6654b586b72f8387ec9fe93c7efd71f8d
Martin Andrews <GitHub@mdda.net>
2019-01-01 08:48:42 +0800

    context may hold useful information, but difficult to inspect

--- src/extension.ts
+++ src/extension.ts
@@ -4,5 +4,9 @@ import * as vscode from "vscode";
 import { ProjectTreeProvider } from "./projectTree";
 
 export function activate(context: vscode.ExtensionContext) {
+  //console.log(context);
+  Object.keys(context).forEach( k => {
+    console.log(k, context[k]);
+  });
   const projectTreeProvider = new ProjectTreeProvider(context);
 }
