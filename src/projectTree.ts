3d2a67aceb096914afaa36dcdf15a4cecaa72595
Martin Andrews <GitHub@mdda.net>
2019-12-04 00:05:19 +0800

    Successfully found config_dir

--- src/projectTree.ts
+++ src/projectTree.ts
@@ -2,19 +2,33 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
+const config_dir_choices = ['./.editor', './.geany', ];
+
 export class ProjectTreeProvider implements vscode.TreeDataProvider<ProjectElement> {
   private _onDidChangeTreeData: vscode.EventEmitter<ProjectElement | undefined> = new vscode.EventEmitter<ProjectElement | undefined>();
   readonly onDidChangeTreeData: vscode.Event<ProjectElement | undefined> = this._onDidChangeTreeData.event;
 
   private test_config: string = "";
   private config_dir: string = undefined;
-
+  
   constructor(private vscode_launch_directory: string) {
     console.log("vscode_launch_directory", vscode_launch_directory);
     
     // unfortutely, this is un-parsed...
     this.test_config = vscode.workspace.getConfiguration('projectTree').get('paths');
     console.log("this.test_config", this.test_config);
+    
+    if(this.pathExists(vscode_launch_directory)) {
+      console.log("Finding config");
+      var valid_dirs = config_dir_choices.map( dir => path.join(vscode_launch_directory, dir)) .filter( dir => this.pathExists(dir) );
+      console.log("valid_dirs", valid_dirs);
+      if(valid_dirs.length>0) {
+        this.config_dir = valid_dirs[0];
+        
+        // Read in the config...
+        
+      }
+    }
   }
 
   refresh(): void {
